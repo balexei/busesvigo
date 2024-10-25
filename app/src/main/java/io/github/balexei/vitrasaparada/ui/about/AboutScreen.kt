7b008bace63c0b2f6435f7fb9cfc21108d1c0f3d
Alexei Belikov <11338016+balexei@users.noreply.github.com>
2024-10-25 18:28:26 +0200

    Add initial about screen implementation

--- app/src/main/java/io/github/balexei/vitrasaparada/ui/about/AboutScreen.kt
+++ app/src/main/java/io/github/balexei/vitrasaparada/ui/about/AboutScreen.kt
@@ -1,9 +1,32 @@
 package io.github.balexei.vitrasaparada.ui.about
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import io.github.balexei.vitrasaparada.R
 
 @Composable
 fun AboutScreen() {
-    Text("About")
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(R.mipmap.ic_launcher_foreground), contentDescription = "App icon"
+        )
+        Text("Vitrasa Parada")
+        Spacer(modifier = Modifier.height(32.dp))
+        Text("Fuente de los datos: Ayuntamiento de Vigo")
+        Text("https://datos.vigo.org")
+    }
 }
